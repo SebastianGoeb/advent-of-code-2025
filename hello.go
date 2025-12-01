fac46d9f608c12fad6b5e52d3ffe9a4d0a71b3f2
Sebastian Göb <sebastiancgoeb@gmail.com>
2025-12-01 23:15:34 +0100

    feat: add error handling

--- hello.go
+++ hello.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/SebastianGoeb/advent-of-code-2025/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Gladys")
+	// configure logger
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// request a greeting message
+	message, err  := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
